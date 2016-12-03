1de201a0c31bbe5f00cabd1a0801d786ef0159bc
Juteau François <f.juteau@gmail.com>
2016-12-03 09:57:11 -0500

    Initial Commit

---
+++ FiveWeather/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FiveWeather
+//
+//  Created by François Juteau on 03/12/2016.
+//  Copyright © 2016 François Juteau. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
